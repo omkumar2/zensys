7e199de13eb86900db35d3a694ba101aaa75e906
Rei WuZen <reiwuzen@gmail.com>
2026-02-07 22:57:06 +0530

    refactor(structure): rework switching logic on reload

--- src/workspace/structure/structure.tsx
+++ src/workspace/structure/structure.tsx
@@ -1,20 +1,18 @@
 import "./structure.scss";
 import { useState } from "react";
 import StructureList from "./structure_list/structure_list";
-// import { priorityBand } from "./helper";
 import { Tag } from "@/types/tag";
 import StructureDetails from "./structure_details/structure_details";
 import StructureCreate from "./structure_create/structure_create";
 import { useTags } from "@/hooks/useTag";
 import { useActiveTab } from "@/hooks/useActiveTab";
-// import {useTags} from "@/hooks/useTag";
 
 const Structure = () => {
   const [tag, setTag] = useState<Tag | null>(null);
   const {activeTab, setActiveTabTypeAndView} = useActiveTab();
   // const [view, setView] = useState<"list" | "details" | "create">("list");
 
-  const { saveTag, reload, removeTag } = useTags();
+  const { saveTag, reloadTags: reload, removeTag } = useTags();
   if(!activeTab) return
   return (
     <div className="structure">
@@ -45,7 +43,7 @@ const Structure = () => {
         />
       )}
       <button className="onClick_create_structure" onClick={() => setActiveTabTypeAndView('structure','add')}>+</button>
-      <button className="onClick_reload_structure" onClick={async () => await reload()}>RE</button>
+      <button className="onClick_reload_structure" onClick={async () => {await reload(); setActiveTabTypeAndView('structure','list')}}>RE</button>
     </div>
   );
 };
