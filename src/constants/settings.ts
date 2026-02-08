4c272d1a661b8951adb807200c56b0f1d92999a4
Rei WuZen <reiwuzen@gmail.com>
2026-02-08 19:29:53 +0530

    feat(settings): add default settings and useSettings hook for application configuration

---
+++ src/constants/settings.ts
@@ -0,0 +1,10 @@
+import { DefaultSettings } from "@/types/settings";
+
+
+export const DEFAULT_SETTINGS : DefaultSettings= {
+    theme: 'dark',
+    memoryView: 'timeline',
+    localOnlyMode: true,
+    debugsLog: false,
+    analytics: false
+}
---
+++ src/types/settings.ts
@@ -0,0 +1,28 @@
+type Theme ='light' | 'dark' | 'blueGrey' | 'system' | 'custom'
+type MemoryView= 'timeline' | 'tree' | 'list'
+
+
+export type StoreState = {
+    theme: Theme
+    setTheme: (t: Theme) => void
+
+    memoryView: MemoryView
+    setMemoryView: (mv: MemoryView) => void
+
+    localOnlyMode: boolean
+    setLocalOnlyMode: (b: boolean)=>void
+
+    debugsLog: boolean,
+    setDebugsLog: (dl: boolean) => void
+
+    analytics: boolean,
+    setAnalytics: (b:boolean) => void
+
+    resetSettings: () => void
+}
+
+type NonFunctionKeys<T> = {
+  [K in keyof T]: T[K] extends Function ? never : K
+}[keyof T];
+
+export type DefaultSettings = Pick<StoreState, NonFunctionKeys<StoreState>>;
